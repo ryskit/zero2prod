5993ac98f617019e80a2aa2d279438079ecaeac4
ryskit <ryosuke.kida@zozo.com>
2022-05-29 00:21:11 +0900

    done ~ 7.2.4

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -4,6 +4,7 @@ use sqlx::{Connection, PgConnection, PgPool};
 
 use once_cell::sync::Lazy;
 use zero2prod::configuration::{get_configuration, DatabaseSettings};
+use zero2prod::email_client::EmailClient;
 use zero2prod::startup::run;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
@@ -43,7 +44,14 @@ async fn spawn_app() -> TestApp {
     configuration.database.database_name = uuid::Uuid::new_v4().to_string();
     let connection_pool = configure_database(&configuration.database).await;
 
-    let server = run(listener, connection_pool.clone()).expect("Failed to bind address");
+    let sender_email = configuration.email_client.sender()
+        .expect("Invalid sender email address.");
+    let email_client = EmailClient::new(
+        configuration.email_client.base_url,
+        sender_email,
+        configuration.email_client.authorization_token);
+
+    let server = run(listener, connection_pool.clone(), email_client.clone()).expect("Failed to bind address");
     let _ = tokio::spawn(server);
     TestApp {
         address,
